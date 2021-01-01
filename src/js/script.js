85e3fa0edddf15e7ad6604082788152cf8c83364
Andrei911 <andrey_gk911@mail.ru>
2021-01-01 23:07:58 +0100

    new JS

--- src/js/script.js
+++ src/js/script.js
@@ -146,27 +146,27 @@
 // console.log(parseInt(twelwe)); //возврат целого чилса 
 // console.log(parseFloat(twelwe));//возврат десятичного числа 
 
-let money,time;
+// let money,time;
 
-function start () {
-	money = +prompt ("Ваш бюджет на месяц?", "");
-	time = +prompt ("Введите дату в формате YYYY-MM-DD", "");
+// function start () {
+// 	money = +prompt ("Ваш бюджет на месяц?", "");
+// 	time = +prompt ("Введите дату в формате YYYY-MM-DD", "");
 
-	while ( isNaN (money) || money == " " || money == null ) {
-		money = +prompt ("Ваш бюджет на месяц?", "");
-	}
-}
+// 	while ( isNaN (money) || money == " " || money == null ) {
+// 		money = +prompt ("Ваш бюджет на месяц?", "");
+// 	}
+// }
 
-start();
+// start();
 
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: true
-};
+// let appData = {
+//     budget: money,
+//     timeData: time,
+//     expenses: {},
+//     optionalExpenses: {},
+//     income: [],
+//     savings: true
+// };
 
 // function choseeExpenses() {
 // 	for (let i = 0; i < 2; i++) {
@@ -186,48 +186,126 @@ let appData = {
 // choseeExpenses();
 
 
-function detectedDayBudget() {
-	appData.moneyPerDay = (appData.budget / 30).toFixed(); //toFixed()округляет значение , 
-// меняет значение на строку 
-	alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");	
-}
+// function detectedDayBudget() {
+// 	appData.moneyPerDay = (appData.budget / 30).toFixed(); //toFixed()округляет значение , 
+// // меняет значение на строку 
+// 	alert ("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");	
+// }
 
-detectedDayBudget();
+// detectedDayBudget();
 
-function detectedLevel(){
-	if (appData.moneyPerDay < 100) {
-		alert("Это минимальный уровень достатка!");
-	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-		alert("Это средний уровень достатка!");
-	} else if (appData.moneyPerDay > 2000) {
-		alert("Это высокий уровень достатка!");
-	} else {
-		alert("Произошла ошибка");
-	}
-}
+// function detectedLevel(){
+// 	if (appData.moneyPerDay < 100) {
+// 		alert("Это минимальный уровень достатка!");
+// 	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+// 		alert("Это средний уровень достатка!");
+// 	} else if (appData.moneyPerDay > 2000) {
+// 		alert("Это высокий уровень достатка!");
+// 	} else {
+// 		alert("Произошла ошибка");
+// 	}
+// }
 
-detectedLevel();
+// detectedLevel();
 
-function checkSavings (){
-	if ( appData.savings == true) {
-		let save = +prompt("Какова сумма накоплений ?"),
-			percent = +prompt("Под какой процент ? ");
+// function checkSavings (){
+// 	if ( appData.savings == true) {
+// 		let save = +prompt("Какова сумма накоплений ?"),
+// 			percent = +prompt("Под какой процент ? ");
 		
-		appData.monthIncome = save/100/12*percent;
-		alert("Ваш доход в месяц составил : " + appData.monthIncome + " руб.," );
-	}
+// 		appData.monthIncome = save/100/12*percent;
+// 		alert("Ваш доход в месяц составил : " + appData.monthIncome + " руб.," );
+// 	}
 	
+// }
+// checkSavings();
+
+
+// function chooseOptExpenses() {
+// 	for (let i = 1; i <= 3; i++) {
+//         let questionOptExpenses = +prompt("Статья необязательных расходов?");
+//         appData.optionalExpenses[i] = questionOptExpenses;
+//         console.log(appData.optionalExpenses);
+//     }
+// }
+
+// chooseOptExpenses();
+
+
+// function first() {
+// 	setTimeout(function(){
+// 		console.log(1);
+// 	}, 500 );
+// }
+
+// function second(){
+// 	console.log(2);
+// }
+
+// first();
+// second();
+
+
+// function learnJS (lang,callback ){
+// 	console.log("Я учу " + lang);
+// 	callback();
+// }
+
+// learnJS("JavaScript", function(){
+// 	console.log("Я прошел 3-ий урок!");
+// });
+
+
+// //объекты 
+
+// let options  = {
+// 	width : 1024 ,
+// 	height : 1024 , 
+// 	name : "test"
+// };
+
+// console.log(options.name);
+
+
+// options.bool = true ; // передать значение в объект 
+// console.log(options.bool);
+// options.colors = {    // запись объекта в объект 
+// 	border : "black",
+// 	bg: "red" 
+// };
+
+// delete options.bool ;  // удалить свойства из объекта 
+
+// console.log(options);
+
+// //ключ объекта
+// for (let key in options){
+// 	console.log("Свойство " + key + " имеет значение " + options[key]);
+// } //перебор элементов объекта 
+
+// console.log(Object.keys(options).length); //количество свойств в объекте 
+
+let arr = [1, 2, 3, 4, 5];
+arr.pop(); //удалить значение с конца 
+
+arr.push(6); //добавляет значение в конце массива 
+arr.push(9);
+console.log(arr); //удалить значение с конца 
+
+arr.shift();//удаляет первый элемент из МАССИВА
+
+console.log(arr); 
+arr.unshift(2); // добавляет элемент в начале МАССИВА
+
+console.log(arr); 
+
+
+// перебор элементов массива 
+
+for(let i = 0; i < arr.length ; i++) {
+	console.log(arr[i]);
+
 }
-checkSavings();
 
-
-function chooseOptExpenses() {
-	for (let i = 1; i <= 3; i++) {
-        let questionOptExpenses = +prompt("Статья необязательных расходов?");
-        appData.optionalExpenses[i] = questionOptExpenses;
-        console.log(appData.optionalExpenses);
-    }
-}
-
-chooseOptExpenses();
-
+arr[99] = 99; 
+arr[99] = 99; //назначение 99 элемента значением 99 . 
